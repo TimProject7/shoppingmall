d3cd10cb7d1c603712d244d54dc4839c6f5b0421
HeeYul Bea <skysinin20@naver.com>
2017-04-26 16:30:15 +0900

    뀨뀨

--- shoppingmall/src/shop.java
+++ shoppingmall/src/shop.java
@@ -4,6 +4,7 @@ public static void main(String[] args) {
 	System.out.println("ㅁㄴㅇ");
 	System.out.print("ㅁㄴㅇㅁ");
 	System.out.println("abcdefg");
-	System.out.println("hijklmn");
+	System.out.println("뀨뀨뀨뀨뀨뀨뀨뀨뀨");
+	System.out.println();
 }
 }
