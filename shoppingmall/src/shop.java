f4917f3a5d5bc8075ebccded7a52d16e72b727eb
jeon won uk <vpzj1234@gmail.com>
2017-04-26 16:03:21 +0900

    테스트

--- shoppingmall/src/shop.java
+++ shoppingmall/src/shop.java
@@ -1,8 +1,8 @@
 
 public class shop {
 public static void main(String[] args) {
-	System.out.println("�눥�븨");
-	System.out.print("�눥�븨�눥�븨");
+	System.out.println("ㅁㄴㅇ");
+	System.out.print("ㅁㄴㅇㅁ");
 	System.out.println("abcdefg");
 	System.out.println("hijklmn");
 }
