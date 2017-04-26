3aec3d98d43d960bb036f44db9af6d4da84762f6
yongho <osg2977@gmail.com>
2017-04-26 16:22:33 +0900

    111

---
+++ shoppingmall/src/shop2.java
@@ -0,0 +1,9 @@
+
+public class shop2 {
+public static void main(String[] args) {
+	System.out.println("ㅁㄴㅇ");
+	System.out.print("ㅁㄴㅇㅁ");
+	System.out.println("abcdefg");
+	System.out.println("hijklmn");
+}
+}
