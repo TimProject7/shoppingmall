b653d6aacae3589756af0b64a0bcfcca77b26735
jeon won uk <vpzj1234@gmail.com>
2017-04-26 15:54:13 +0900

    jeon

--- shoppingmall/src/shop.java
+++ shoppingmall/src/shop.java
@@ -1,7 +1,8 @@
 
 public class shop {
 public static void main(String[] args) {
-	System.out.println("쇼핑");
-	System.out.print("쇼핑쇼핑");
+	System.out.println("�눥�븨");
+	System.out.print("�눥�븨�눥�븨");
+	System.out.println("abcdefg");
 }
 }
