cbb6f549c4156fdd018bf1075ee14891c87a0d42
YongWon Kim <kimdyddnjs@naver.com>
2017-04-26 16:31:45 +0900

    초기설정 완료, 코딩 시작

--- shoppingmall/src/shop2.java
+++
@@ -1,9 +0,0 @@
-
-public class shop2 {
-public static void main(String[] args) {
-	System.out.println("ㅁㄴㅇ");
-	System.out.print("ㅁㄴㅇㅁ");
-	System.out.println("abcdefg");
-	System.out.println("hijklmn");
-}
-}
