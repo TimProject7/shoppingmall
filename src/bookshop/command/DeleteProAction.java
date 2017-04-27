67212e9ee714fd2114aaa8ee735f98d170d4ba18
HeeYul Bea <skysinin20@naver.com>
2017-04-27 15:11:17 +0900

    딜리트까지 완료

--- src/bookshop/command/DeleteProAction.java
+++ src/bookshop/command/DeleteProAction.java
@@ -3,12 +3,24 @@
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bookshop.bean.LogonDBBean;
+
 
 
 public class DeleteProAction implements CommandAction {
 
 	@Override
 	public String requestPro(HttpServletRequest request,HttpServletResponse response)throws Throwable{
-		return null;
+		
+		String id = request.getParameter("id");
+		String passwd = request.getParameter("passwd");
+		
+		//사용자가 입력한 id, passwd 를 가지고 회원 정보 삭제 후 성공 여부 반환
+		LogonDBBean manager = LogonDBBean.getinstance();
+		int check = manager.deleteMember(id, passwd);
+		
+		request.setAttribute("check", new Integer(check));
+		
+		return "/member/deletePro.jsp";
 	}
 }
--- src/bookshop/command/ModifyProAction.java
+++ src/bookshop/command/ModifyProAction.java
@@ -3,10 +3,31 @@
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bookshop.bean.LogonDBBean;
+import bookshop.bean.LogonDataBean;
+
 public class ModifyProAction implements CommandAction {
 
 	@Override
 	public String requestPro(HttpServletRequest request,HttpServletResponse response)throws Throwable{
-		return null;
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		//수정할 회원 정보
+		LogonDataBean member = new LogonDataBean();
+		member.setId(request.getParameter("id"));
+		member.setPasswd(request.getParameter("passwd"));
+		member.setName(request.getParameter("name"));
+		member.setAddress(request.getParameter("address"));
+		member.setTel(request.getParameter("tel"));
+		
+		//수정할 회원 정보를 가지고 수정 처리 후 성공 여부 반환
+		LogonDBBean manager = LogonDBBean.getinstance();
+		
+		int check = manager.updateMember(member);
+		
+		request.setAttribute("check", new Integer(check));
+		
+		return "/member/modifyPro.jsp";
 	}
 }
