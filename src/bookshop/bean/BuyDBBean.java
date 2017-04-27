cbdec2e90926dd3753d63a9764a56e9e6d5fcea8
jeon won uk <vpzj1234@gmail.com>
2017-04-27 16:07:43 +0900

    목록 리스트

--- src/bookshop/bean/BuyDBBean.java
+++ src/bookshop/bean/BuyDBBean.java
@@ -1,5 +1,14 @@
 ﻿package bookshop.bean;
 
 public class BuyDBBean {
-   
+	private static BuyDBBean instance = new BuyDBBean();
+
+	// LogonDBBean객체를 리턴하는 메소드
+	public static BuyDBBean getinstance() {
+		return instance;
+	}
+
+	private BuyDBBean() {
+
+	}
 }
