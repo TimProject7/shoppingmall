44c766f8a906305dd5724dff856df3e45b137a8d
yongho <osg2977@gmail.com>
2017-04-27 10:14:27 +0900

    ManagerMainAction

--- bookshop/command/ManagerMainAction.java
+++ bookshop/command/ManagerMainAction.java
@@ -1,6 +1,11 @@
 package bookshop.command;
 
-import bookshop.process.CommandAction;
 public class ManagerMainAction implements CommandAction {
-
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable{
+		
+		//관리자를 구분할 때 사용
+		request.setAttribute("type", new Integer(0));
+		return "/mngr/managerMain.jsp";//응답 페이지
+	}
 }
