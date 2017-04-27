0878a0e733879ef518d0f5cac230412e237cf1fb
YongWon Kim <kimdyddnjs@naver.com>
2017-04-27 09:11:38 +0900

    다시시작

--- shoppingmall/src/bookshop/bean/CommandAction.java
+++
@@ -1,9 +0,0 @@
-package bookshop.bean;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface CommandAction {
-	public String requestPro(HttpServletRequest request, HttpServletResponse Response) throws Throwable;
-
-}
---
+++ shoppingmall/src/bookshop/command/QnaReplyProAction.java
@@ -0,0 +1,16 @@
+﻿package bookshop.command;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class QnaReplyProAction implements CommandAction {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable{
+		
+		return null;
+	}
+
+}
