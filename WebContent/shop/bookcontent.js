0615aeeb9301427db913fd1238bf8f37be619932
YongWon Kim <kimdyddnjs@naver.com>
2017-04-27 13:15:56 +0900

    bookshop.command
    BookContentAction.java
    ProListAction.java
    ShopMainAction.java
    
    
    Webcontent.shop
    
    shopMain.jsp
    showList.jsp

--- WebContent/shop/bookcontent.js
+++ WebContent/shop/bookcontent.js
@@ -1,1 +1,16 @@
-﻿
+﻿$(document).ready(function() {
+	$("#insertCart").click(function() { // [장바구니에 담기] 버튼 클릭
+		var buyer = $("$#buyer").val();
+		var book_kind = $("#book_kind").val();
+		var query = {
+			book_id : $("#book_id").val(),
+			buy_count : $("#buy_count").val(),
+			book_image : $("#book_image").val(),
+			book_title : $("book_title").val(),
+			buy_price : $("#buy_price").val(),
+			buyer : buyer
+		};
+		$.ajax({})
+
+	})
+})
