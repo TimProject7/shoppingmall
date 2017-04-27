b1844b0d149223143ba5d6a87f46b9c30961c76c
YongWon Kim <kimdyddnjs@naver.com>
2017-04-27 16:06:37 +0900

    qna까지

--- src/bookshop/command/QnaDeleteProAction.java
+++ src/bookshop/command/QnaDeleteProAction.java
@@ -3,12 +3,22 @@
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+import bookshop.bean.QnaDBBean;
 
 public class QnaDeleteProAction implements CommandAction {
 
 	@Override
-	public String requestPro(HttpServletRequest request,HttpServletResponse response)throws Throwable{
-		return null;
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		request.setCharacterEncoding("utf-8");
+
+		int qna_id = Integer.parseInt(request.getParameter("qna_id"));
+
+		// qna_id에 해당하는 qna 삭제
+		QnaDBBean qnaProcess = QnaDBBean.getInstance();
+		int check = qnaProcess.deleteArticle(qna_id);
+
+		request.setAttribute("check", new Integer(check));
+
+		return "qna/qnaDeletePro.jsp";
 	}
 }
