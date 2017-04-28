1612d0635b5f63c279bdeef3dc6403b87dc3bfe8
YongWon Kim <kimdyddnjs@naver.com>
2017-04-28 16:08:50 +0900

    z

--- WebContent/mngr/logon/mlogin.js
+++ WebContent/mngr/logon/mlogin.js
@@ -1,30 +1,32 @@
-﻿$(document).ready(function(){
-	//[로그인] 버튼을 클릭하면 자동실행
-	$("#login").click(function(){
-		var query={id:$("#id").val(),
-				passwd:$("#passwd").val()};
-		
+﻿$(document).ready(function() {
+	// [로그인] 버튼을 클릭하면 자동실행
+	$("#login").click(function() {
+		var query = {
+			id : $("#id").val(),
+			passwd : $("#passwd").val()
+		};
+
 		$.ajax({
-			type:"post",
-			url:"/shoppingmall/mg/managerLoginPro.do",
-			data:query,
-			success:function(data){
+			type : "post",
+			url : "/shoppingmall/mg/managerLoginPro.do",
+			data : query,
+			success : function(data) {
 				location.reload(true);
-				//location.href="/shoppingmall/mg/managerMain.do";
+				// location.href="/shoppingmall/mg/managerMain.do";
 			}
 		});
-	});//end login
-	
-	//[로그아웃]버튼을 클릭하면 자동실행
-	$("#logout").click(function(){
+	});// end login
+
+	// [로그아웃]버튼을 클릭하면 자동실행
+	$("#logout").click(function() {
 		$.ajax({
-			type:"post",
-			url:"/shoppingmall/mg/managerLogout.do",
-			success:function(data){
+			type : "post",
+			url : "/shoppingmall/mg/managerLogout.do",
+			success : function(data) {
 				location.reload(true);
-				//location.reload("/shoppingmall/mg/managerMain.do");
+				// location.reload("/shoppingmall/mg/managerMain.do");
 			}
 		});
-	});//end logout
-	
+	});// end logout
+
 });
