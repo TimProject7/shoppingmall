79adf5e70e62cb0654447f13f1265bf4251ca028
yongho <osg2977@gmail.com>
2017-04-28 13:31:07 +0900

    오타수정

--- WebContent/mngr/productProcess/bookregist.js
+++ WebContent/mngr/productProcess/bookregist.js
@@ -1,7 +1,7 @@
-﻿$(document).ready(function() { //[책등록] 버튼 클릭
+﻿$(document).ready(function(){ //[책등록] 버튼 클릭
 	$("#upForm1").ajaxForm({ //이미지를 포함한 상품 등록
 		success : function(data, status) { //업로드에 성공하면 수행
-			location.href("/shoppingmaill/mg/bookList.do?book_kind=all");
+			location.href("/shoppingmall/mg/bookList.do?book_kind=all");
 		}
 	});
 
