059187f9bf4662edc775c9668c80058078994917
jeon won uk <vpzj1234@gmail.com>
2017-04-28 14:35:51 +0900

    asd

--- WebContent/mngr/productProcess/booklist.js
+++ WebContent/mngr/productProcess/booklist.js
@@ -1,11 +1,12 @@
 ﻿$(document).ready(function(){
-	
+	alert("왜안되니")
 	$("#regist").click(function(){//[책등록] 버튼 클릭
-		location.href("/shoppingmall/mg/bookRegisterForm.do");
+		location.href="/shoppingmall/mg/bookRegisterForm.do";
 		
 	});
 	$("#bookMain").click(function(){//[관리자 메인으로] 버튼클릭
-		location.href("/shoppingmall/mg/managerMain.do");
+		alert("왜안되니")
+		location.href="/shoppingmall/mg/managerMain.do";
 	});
 });
 
@@ -15,7 +16,7 @@ function edit(editBtn){
 	var arr = rStr.split(",");
 	var query = "/shoppingmall/mg/bookUpdateForm.do?book_id="+arr[0];
 	query += "&book_kind="+arr[1];
-	location.href(query);
+	location.href=query;
 }
 
 //[삭제]버튼을 클릭하면 자동 실행
@@ -24,5 +25,5 @@ function del(delBtn){
 	var arr = rStr.split(",");
 	var query = "/shoppingmll/mg/bookDeletePro.do?book_id="+arr[0];
 	query += "$&book_kin="+arr[1];
-	location.href(query);
+	location.href=query;
 }
