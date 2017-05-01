f432a3e0742a2cc65924492c8caacd963a3572b0
jeon won uk <vpzj1234@gmail.com>
2017-05-01 09:33:28 +0900

    에러

--- src/bookshop/command/DeleteProAction.java
+++ src/bookshop/command/DeleteProAction.java
@@ -10,16 +10,21 @@ import bookshop.bean.LogonDBBean;
 public class DeleteProAction implements CommandAction {
 
 	@Override
-	public String requestPro(HttpServletRequest request,HttpServletResponse response)throws Throwable{
+	public String requestPro(HttpServletRequest request,HttpServletResponse response){
+		try{
+			String id = request.getParameter("id");
+			String passwd = request.getParameter("passwd");
+			
+			//사용자가 입력한 id, passwd 를 가지고 회원 정보 삭제 후 성공 여부 반환
+			LogonDBBean manager = LogonDBBean.getinstance();
+			int check = manager.deleteMember(id, passwd);
+			
+			request.setAttribute("check", new Integer(check));
+		}catch (Exception e) {
+			System.out.println("DeleteProAction 에러 :");
+			e.printStackTrace();
+		}
 		
-		String id = request.getParameter("id");
-		String passwd = request.getParameter("passwd");
-		
-		//사용자가 입력한 id, passwd 를 가지고 회원 정보 삭제 후 성공 여부 반환
-		LogonDBBean manager = LogonDBBean.getinstance();
-		int check = manager.deleteMember(id, passwd);
-		
-		request.setAttribute("check", new Integer(check));
 		
 		return "/member/deletePro.jsp";
 	}
